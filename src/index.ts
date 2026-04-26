94fc569a05aa1053c4a5a7fd59449977e0d2562e
shazaniyu55 <shazaniyu@gmail.com>
2026-04-26 19:14:57 +0100

    'vercel24'

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import morgan from "morgan";
  import indexRouter from "./routes/index.js";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(helmet());
 app.use(express.json());
