a0c14516293c2934a845175e3e574900e0de8cf7
shazaniyu55 <shazaniyu@gmail.com>
2026-04-26 19:32:30 +0100

    'vercel243'

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,27 +4,32 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Create a typed connection pool
-const pool: Pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: false // set to true if using a remote DB like Render, Supabase, etc.
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  // required for Supabase
 });
 
-// Immediately Invoked Async Function to test the DB connection
-(async () => {
-  try {
-    const client = await pool.connect();
-    console.log('PostgreSQL connected successfully');
-    client.release(); // release the client back to the pool
-  } catch (err: any) {
-    console.error('Database connection failed:', err.message);
-    process.exit(1);
-  }
-})();
+// Create a typed connection pool
+// const pool: Pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: false // set to true if using a remote DB like Render, Supabase, etc.
+// });
 
-// Export the pool for usage in other files
-export default pool;
+// // Immediately Invoked Async Function to test the DB connection
+// (async () => {
+//   try {
+//     const client = await pool.connect();
+//     console.log('PostgreSQL connected successfully');
+//     client.release(); // release the client back to the pool
+//   } catch (err: any) {
+//     console.error('Database connection failed:', err.message);
+//     process.exit(1);
+//   }
+// })();
+
+// // Export the pool for usage in other files
+// export default pool;
